7cb73ebff0768a46dd0f0305942305efd22a296d
md-kh <khouloudmaddouri01@gmail.com>
2021-07-02 15:16:48 +0200

    insert values

--- DDL.sql
+++ DDL.sql
@@ -24,3 +24,11 @@ CREATE TABLE Customers(
 
 Alter TABLE Product ADD Category VARCHAR2(20);
 Alter TABLE Orders ADD OrderDate DATE(SYSDATE);
+ INSERT INTO Product (Product_id, product_name, Category, Price)VALUES ('PO1', 'Samsung Galaxy S20', 'Smartphone', 3299);
+INSERT INTO Product (Product_id, product_name, Category, Price)VALUES ('PO2', 'Asus Notebook', 'Pc', 4599);
+
+INSERT INTO Orders (Customer_id, Product_id, OrderDate, quantity, total_amoutn)VALUES ('C01', 'P01', NULL, 2, 9198);
+INSERT INTO Orders (Customer_id, Product_id, OrderDate, quantity, total_amoutn)VALUES ('C02', 'P02', 28/05/2020, 1, 3299);
+
+INSERT INTO Customers (Customer_id, Customer_name, Customer_Tel)VALUES ('C01', 'Ali', 7321009);
+INSERT INTO Customers (Customer_id, Customer_name, Customer_Tel)VALUES ('C02', 'Asma', 77345823);
